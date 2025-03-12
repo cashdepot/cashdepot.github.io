06d9076785673607912b1064600450f552ccb87e
Cash Depot <116501583+cashdepot@users.noreply.github.com>
2025-03-12 11:22:29 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,1 +0,0 @@
-sudo sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config && sudo sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config  && sudo service sshd restart
